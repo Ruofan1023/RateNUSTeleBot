a0667880e3765e0feae4d2f3396d410bcd14e4a2
Ruofan1023 <69947227+Ruofan1023@users.noreply.github.com>
2022-01-07 15:23:38 +0800

    add food handler

---
+++ stall_list_commmand_handler.py
@@ -0,0 +1,25 @@
+import requests
+from telegram.ext import *
+
+url = 'https://www.ratenus.cyou:8080/stall'
+
+
+def stall_list_command(update, context):
+    body = {
+        "orderBy": "id",
+        "isLowToHigh": True,
+        "pageNum": 0,
+        "pageSize": 100
+    }
+    response = requests.post(url, json=body)
+    print(response.json())
+    response_json = response.json()
+    name_list = []
+    content_list = response_json['content']
+    for food in content_list:
+        name_list.append(str(food['id']) + ". " + food['name'] + "\n")
+    update.message.reply_text("Which restaurant would you like to see?\nReply with /food + id :)")
+    update.message.reply_text("".join(name_list))
+
+
+stall_list_command_handler = CommandHandler("foods", stall_list_command)
